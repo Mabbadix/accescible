4388bdcd0cbccb8ec0457aa0a9ac52787253fbcb
Abbadix <devabbadimehdi@gmail.com>
2016-12-22 08:22:33 +0100

    Validation Head.php

--- head.php
+++ head.php
@@ -1,11 +1,11 @@
     <meta charset=utf-8 />
     <meta http-equiv="Content-Language" content="fr"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes"/>
-    <meta name="description" content="Informer les autres de problemes d'accessibilité près de chez vous"/>
+    <meta name="description" content="Signaler les problemes d'accessibilité près de chez vous afin que les personnes compétentes y remédient."/>
     <meta name="robots" content="index, follow"/>
     <meta name="Author" content="Mehdi Abbadi, Dylan Bourdere Andreou, Design Emmanuelle Brasselle">
     <link rel="stylesheet" type="text/css" href="css/style.css" media="screen"/>
     <link rel="shortcut icon" href="img/logo7.png"/>
     <link rel="icon" type="image/x-icon" sizes="16x16" href="img/logo7.png"/>
     <link rel="icon" href="img/logo7.png" sizes="any" type="image/svg">
-    <link rel="apple-touch-icon" sizes="16x16" href="img/logo7.png"/>   
+    <link rel="apple-touch-icon" sizes="16x16" href="img/logo7.png"/>
