155f6d72cadd2a2d412ed86086b4166cf6c99113
loperiane <loperanes@gmail.com>
2017-01-20 12:48:54 +0100

    compte.php

---
+++ compte.php
@@ -0,0 +1,18 @@
+<?php
+session_start();
+  require'UtilisateurManager.php';
+  require'connData.php';
+  $manageU = new UtilisateurManager($bdd);
+  $mail = $_SESSION['emailU'];
+  $mdp = $_SESSION['mdpU'];
+  $nom = $_POST['nomU'];
+  $prenom = $_POST['prenomU'];
+  $adresse = $_POST['adresseU'];
+  $ville = $_POST['villeU'];
+  $cp = $_POST['cpU'];
+  $tel = $_POST['telU'];
+  print_r($_SESSION);
+  echo'ok';
+  $manageU->updateUtilisateur($mail, $nom, $prenom, $adresse, $ville, $cp, $tel);
+  echo'ok';
+?>
