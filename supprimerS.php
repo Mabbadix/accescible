038a819e81f7eb00c620832f6cc644c70c8e59da
Abbadix <devabbadimehdi@gmail.com>
2017-01-30 19:33:49 +0100

    tentative supprime signal

--- supprimerS.php
+++ supprimerS.php
@@ -1,5 +1,15 @@
 <?php
-echo "string";
+/** Importation de l'autoloader **/
 
+require 'Autoloader.php';
+$autoload = new Autoloader;
+$autoload->register();
 
+include 'connData.php';
+
+$id = $_GET['id'];
+echo $id;
+$sm = new SignalementManager($bdd);
+$sm->delete($id);
+header("Location: monCompteBis.php");
  ?>
