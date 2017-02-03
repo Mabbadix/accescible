a22b7d5b2335ae1a7b7166db9c18b25985c23c4c
loperiane <loperanes@gmail.com>
2017-02-03 02:50:33 +0100

    rename

---
+++ compteTraitement.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+/** Importation de l'autoloader **/
+
+require 'class/Autoloader.php';
+$autoload = new Autoloader;
+$autoload->register();
+
+require'connData.php';
+$manageU = new UtilisateurManager($bdd);
+$mail = $_SESSION['emailU'];
+$mdp = $_SESSION['mdpU'];
+$nom = $_POST['nomU'];
+$prenom = $_POST['prenomU'];
+$adresse = $_POST['adresseU'];
+$ville = $_POST['villeU'];
+$cp = $_POST['cpU'];
+$tel = $_POST['telU'];
+print_r($_SESSION);
+echo'ok';
+$manageU->updateUtilisateur($mail, $nom, $prenom, $adresse, $ville, $cp, $tel);
+
+header("Location: monCompteBis.php");
+?>
